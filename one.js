1ec9c5ed574a284ffce9d18b92701c2b68230561
slavenoptimus <opojevlic@gmail.com>
2017-09-20 21:43:03 +0200

    Create one.js

---
+++ one.js
@@ -0,0 +1,26 @@
+function funForMe() {
+  var probability1 = 0;
+  var total;
+  if (probability1 >= 0) {
+    let payback = 95;
+    window.print(total);
+    }
+   }
+   
+   var probability1 = prompt();
+   var payback;
+   
+   switch (probability1) {
+
+    case 50:
+      payback = 94;
+      console.log( total = 1 / probability1 * payback);
+        break;
+
+    case 65:
+      payback = 90;
+      console.log( total = 1 / probability1 * payback);
+        break;
+    default:
+      ...
+   }
